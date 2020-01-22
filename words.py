6e8c51099cbfb40a8cb80eef3905418d9e7af02d
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2020-01-22 19:58:13 +0200

    Implement to_transcription

--- words.py
+++ words.py
@@ -22,53 +22,54 @@ def get_sheet_name(lang: Language):
     sheets = list()
     xlsheets = pe.get_book(file_name=c.XL_FILE).sheet_names()
 
-    for work_sheet in sheet_can_include:
-        [sheets.append(sheet) for sheet in xlsheets if re.search(
-            work_sheet.lower(), sheet.lower())]
-    return sheets[0]
+    try:
+        for work_sheet in sheet_can_include:
+            [sheets.append(sheet) for sheet in xlsheets if re.search(
+                work_sheet.lower(), sheet.lower())]
+        return sheets[0]
+    except:
+        raise NotImplementedError('Sheet is not exist')
 
 
 def to_transcript(words):
-    # try find solution to non alpabetic symbols
     my_vocabulary = dict()
     for entry in words:
-        word = entry[c.ENTRY_PART['word']]
-        if re.findall('[a-zA-Z]', word):
-            transcript = re.sub(
-                r"\[{0,}\]{0,}", "", entry[c.ENTRY_PART['transcription']])
-            d = word.split()
-            if len(d) > 1:
-                t = transcript.split()
-                for i, j in enumerate(d):
-                    my_vocabulary[j] = t[i]
-            else:
-                my_vocabulary[word] = transcript
-    return my_vocabulary
+        # TODO: change re pattern
+        word = re.sub(r"(-\w+)", "",
+                      entry[c.ENTRY_PART['word']])
+        transcript = re.sub(
+            r"\[*?\]*?", "", entry[c.ENTRY_PART['transcription']])
 
-    # entries = set()
-    # for entry in entry_list:
-    #     [entries.add(word) for word in entry[0].split() if
-    #         re.findall('[a-zA-Z]', word)]
-    # return entries
-    # pprint.pprint(entry_list)
+        word_to_array = word.split()
+        transcript_to_array = transcript.split()
+
+        for i, j in enumerate(word_to_array):
+            if len(word_to_array) > 0 and len(word_to_array) <= len(transcript_to_array):
+                my_vocabulary[j] = transcript_to_array[i]
+            else:
+                if j not in my_vocabulary and re.findall('[a-zA-Z]', j):
+                    my_vocabulary[j] = ''
+
+    pprint.pprint(my_vocabulary)
 
 
 def get_words_dictionary(lang: Language):
-    use_sheet = get_sheet_name(lang)
-    words_dictionary = pe.get_records(file_name=c.XL_FILE, sheet_name=use_sheet,
+    sheet_to_use = get_sheet_name(lang)
+    words_dictionary = pe.get_records(file_name=c.XL_FILE, sheet_name=sheet_to_use,
                                       name_columns_by_row=1)
     return words_dictionary
 
 
 def get_words_list(lang: Language):
-    use_sheet = get_sheet_name(lang)
+    sheet_to_use = get_sheet_name(lang)
     entry_list = pe.get_array(file_name=c.XL_FILE,
-                              sheet_name=use_sheet,
+                              sheet_name=sheet_to_use,
                               start_column=c.XL_TABLE_START_POSITION['column'],
                               start_row=c.XL_TABLE_START_POSITION['row'])
+    # print(entry_list)
     return entry_list
 
 
 if __name__ == '__main__':
     r = get_words_list(Language.English)
-    print(to_transcript(r))
+    to_transcript(r)
